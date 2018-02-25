9638f7d546c50b52f6ee85299335574034b10660
mikachho <36812857+mikachho@users.noreply.github.com>
2018-02-25 19:09:24 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -1,18 +1,12 @@
-console.log("222222222222"+pix.isMobile);	
+console.log("11111"+pix.isMobile);	
 var head = document.getElementsByTagName("head")[0];
 	var style = document.createElement('style');
 	style.type = 'text/css';
 	head.appendChild(style);
 	var tempsheet= style.sheet;
 	tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,.pixnet-ad{ display: none!important; }", 0);
-		if(!typeof str === "undefined" && pix.isMobile){
+		if(!typeof pix === "undefined" && pix.isMobile){
 			var xmk = document.getElementsByClassName("relative-article")[0];
 			xmk.remove(xmk.selectedIndex);
-		}
-		if(!typeof str === "undefined"){
-			console.log("111111111111");
-		}
-		if(pix.isMobile){
-console.log("222222222222"+pix.isMobile);
 		}
 console.log("222222222222"+pix.isMobile);
