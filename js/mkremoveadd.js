34ec93568f709e21e5c0475f4f25977469e9f5ad
mikachho <36812857+mikachho@users.noreply.github.com>
2018-02-25 18:56:33 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -6,6 +6,7 @@
 	tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,.pixnet-ad{ display: none!important; }", 0);
 	window.onload = function(){
 		if(!typeof str === "undefined" && pix.isMobile){
+			console.log("XXXXXXXXXXXXXXXX");
 			var xmk = document.getElementsByClassName("relative-article")[0];
 			xmk.remove(xmk.selectedIndex);
 		}
