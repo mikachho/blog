38db15858630850a0120a22166be5de873ac3d87
mikachho <36812857+mikachho@users.noreply.github.com>
2018-02-25 18:32:43 +0800

    Create mkremoveadd.js

---
+++ js/mkremoveadd.js
@@ -0,0 +1,12 @@
+	var head = document.getElementsByTagName("head")[0];
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	head.appendChild(style);
+	var tempsheet= style.sheet;
+	tempsheet.insertRule(".in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad{ display: none!important }", 0);
+	window.onload = function(){
+		tempsheet.insertRule(".pixnet-ad{ display: none!important }", 0);
+		if(!typeof str === "undefined" && pix.isMobile){
+			tempsheet.insertRule(".pix-related-post,.pix-related-post-showed{ display: none!important }", 0);
+		}
+	};
