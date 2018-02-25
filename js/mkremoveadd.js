9ae34049f02cdbd8c3b58229016da938d0701db4
mikachho <36812857+mikachho@users.noreply.github.com>
2018-02-25 19:12:17 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -1,12 +1,15 @@
-console.log("11111"+pix.isMobile);	
 var head = document.getElementsByTagName("head")[0];
-	var style = document.createElement('style');
-	style.type = 'text/css';
-	head.appendChild(style);
-	var tempsheet= style.sheet;
-	tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,.pixnet-ad{ display: none!important; }", 0);
-		if(!typeof pix === "undefined" && pix.isMobile){
-			var xmk = document.getElementsByClassName("relative-article")[0];
-			xmk.remove(xmk.selectedIndex);
-		}
-console.log("222222222222"+pix.isMobile);
+var style = document.createElement('style');
+style.type = 'text/css';
+head.appendChild(style);
+var tempsheet= style.sheet;
+tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,.pixnet-ad{ display: none!important; }", 0);
+
+window.onload = function () {
+	console.log("11111"+pix.isMobile);
+	if(!typeof pix === "undefined" && pix.isMobile){
+		var xmk = document.getElementsByClassName("relative-article")[0];
+		xmk.remove(xmk.selectedIndex);
+	}
+	console.log("222222222222"+pix.isMobile);
+} 
