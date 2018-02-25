79d110c4c095ebfd4d83c2aa6cb7efe07eb23274
mikachho <36812857+mikachho@users.noreply.github.com>
2018-02-25 18:39:50 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -3,9 +3,9 @@
 	style.type = 'text/css';
 	head.appendChild(style);
 	var tempsheet= style.sheet;
-	tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad{ display: none!important }", 0);
+	tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,.pixnet-ad{ display: none!important }", 0);
 	window.onload = function(){
-		tempsheet.insertRule(".pixnet-ad{ display: none!important }", 0);
+		//tempsheet.insertRule(".pixnet-ad{ display: none!important }", 0);
 		if(!typeof str === "undefined" && pix.isMobile){
 			tempsheet.insertRule(".pix-related-post,.pix-related-post-showed{ display: none!important }", 0);
 		}
