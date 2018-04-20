380cc780a9779d5fe9bcb4e5cef4961bc94da2f6
mikachho <36812857+mikachho@users.noreply.github.com>
2018-04-21 00:57:21 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -6,12 +6,15 @@ var tempsheet= style.sheet;
 tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad{ display: none!important; }", 0);
 tempsheet.insertRule("p,div { word-break:break-all; }", 0);
 
-window.onload = function () {
-	//if(!(typeof pix === "undefined") && pix.isMobile){
-		tempsheet.insertRule(".pix-related-post,.pix-related-post-showed{ display: none!important; }", 0);
-	//}
-	var mkxx = document.getElementsByClassName("pixnet-ad");
-	for (var i=0, j = mkxx.length;i<j;i++){
-	  mkxx[i].style.display = 'none';
-	}
-} 
+//window.onload = function () {
+	try{
+		//if(!(typeof pix === "undefined") && pix.isMobile){
+			tempsheet.insertRule(".pix-related-post,.pix-related-post-showed{ display: none!important; }", 0);
+		//}
+		var mkxx = document.getElementsByClassName("pixnet-ad");
+		for (var i=0, j = mkxx.length;i<j;i++){
+		  mkxx[i].style.display = 'none';
+		}
+	}catch(e){}
+
+//} 
