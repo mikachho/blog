f15b7501dc33ee24200c36a49dc5131906ba1396
mikachho <36812857+mikachho@users.noreply.github.com>
2018-04-21 01:24:51 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -6,21 +6,9 @@ var tempsheet= style.sheet;
 tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad{ display: none!important; }", 0);
 tempsheet.insertRule("p,div { word-break:break-all; }", 0);
 
-/*
-window.onload = function () {
-	try{
-		//if(!(typeof pix === "undefined") && pix.isMobile){
-			tempsheet.insertRule(".pix-related-post,.relative-article{ display: none!important; }", 0);
-		//}
-		var mkxx = document.getElementsByClassName("pixnet-ad");
-		for (var i=0, j = mkxx.length;i<j;i++){
-		  mkxx[i].style.display = 'none';
-		}
-	}catch(e){}
-
-} */
 try{
 	(function() {
+		console.log("pix.isMobile:" + pix.isMobile);
 		if(!(typeof pix === "undefined") && pix.isMobile){
 			tempsheet.insertRule(".pix-related-post,.relative-article{ display: none!important; }", 0);
 		}
