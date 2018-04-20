7b15dc2e9e4dc67908b4f880da963c8142e1002c
mikachho <36812857+mikachho@users.noreply.github.com>
2018-04-21 00:56:16 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -7,9 +7,9 @@ tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.oct
 tempsheet.insertRule("p,div { word-break:break-all; }", 0);
 
 window.onload = function () {
-	if(!(typeof pix === "undefined") && pix.isMobile){
+	//if(!(typeof pix === "undefined") && pix.isMobile){
 		tempsheet.insertRule(".pix-related-post,.pix-related-post-showed{ display: none!important; }", 0);
-	}
+	//}
 	var mkxx = document.getElementsByClassName("pixnet-ad");
 	for (var i=0, j = mkxx.length;i<j;i++){
 	  mkxx[i].style.display = 'none';
