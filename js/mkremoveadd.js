254d0f034a1f27ea7d27c066c8fc8b5f2ac7b7ef
mikachho <36812857+mikachho@users.noreply.github.com>
2021-01-15 15:08:16 +0800

    Update mkremoveadd.js

--- js/mkremoveadd.js
+++ js/mkremoveadd.js
@@ -3,16 +3,19 @@ var style = document.createElement('style');
 style.type = 'text/css';
 head.appendChild(style);
 var tempsheet= style.sheet;
-tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,.pixnet-ad>div,div[id^='div-gpt-ad']:nth-child(n+10),.global-keyword,.article-body:last-child { display: none!important; }", 0);
+tempsheet.insertRule(".header-ad,.in-read-ad,.hot-articles,.discover-stream,.octopus,.article-ad,div[id^='div-gpt-ad']:nth-child(n+10),.global-keyword,.article-body:last-child { display: none!important; }", 0);
 tempsheet.insertRule("p,div { word-break:break-all; }", 0);
 tempsheet.insertRule("#main .header-fixed-area {min-height: unset;}", 0);
 
 
 try{
 	(function() {
+		tempsheet.insertRule(".pixnet-ad { display: none!important; }", 0);
 		if('none' !==document.getElementsByTagName("body")[0].getAttribute("data-mobile-mode")){
 			tempsheet.insertRule(".pix-related-post,.relative-article{ display: none!important; }", 0);
 		}
+		
+
 		
 		/*
 		var artWet = document.getElementsByClassName('article-body')[0];
